ba3d3f67c814219902939c9eba7fb433a01f56c8
David Castel <davidcastelcastells@gmail.com>
2019-03-07 14:59:51 +0100

    Fin seccion 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Routing (Nuestras rutas de la aplicacion).
 import { AppRoutingModule } from './app-routing.module';
+
+// Pages (Las paginas de la aplicacion).
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ItemComponent } from './pages/item/item.component';
+
 
 @NgModule({
   declarations: [
